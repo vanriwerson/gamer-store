1b445f56312d08f8296f9639d7d8fa7ce3ad35e0
Bruno Riwerson Silva <riwerson@gmail.com>
2024-10-04 07:59:31 -0300

    integração db, backend e frontend (turbo e prisma)

--- frontend/src/core/produto/index.ts
+++
@@ -1,9 +0,0 @@
-import Especificacoes from "./Especificacoes"
-import Precificavel from "./Precificavel"
-import Produto from "./Produto"
-
-export type {
-  Especificacoes,
-  Precificavel,
-  Produto
-}
--- frontend/src/core/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import Moeda from './Moeda'
-
-export {
-  Moeda
-}
